d37cae6080f501775c019a17a03c297b53d563a1
Hiroki Chiyoda <contact@arafipro.com>
2024-06-18 16:15:10 +0900

    コードを修正

--- db/schema.ts
+++ db/schema.ts
@@ -2,6 +2,6 @@ import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const stockTable = sqliteTable("stock_table", {
   code: text("code", { length: 4 }).primaryKey(),
-  stockName: text("stock_name", { length: 128 }).notNull(),
-  market: text("market", { length: 32 }).notNull(),
+  stockName: text("stock_name").notNull(),
+  market: text("market").notNull(),
 });
